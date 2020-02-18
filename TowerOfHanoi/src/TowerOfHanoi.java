9fd557840206c9226e37a87482fe653d57f0fa2f
IllusionistBoi <ronit.dahiya@ucdconnect.ie>
2020-02-18 19:12:52 +0000

    Comments And Functionality Improved

--- TowerOfHanoi/src/TowerOfHanoi.java
+++ TowerOfHanoi/src/TowerOfHanoi.java
@@ -1,23 +1,25 @@
 public class TowerOfHanoi {
 
     // Java recursive function to solve tower of hanoi puzzle
-    static void towerOfHanoi(int n, char source, char to_rod, char aux_rod)
+    static void towerOfHanoi(int Dish_Number, char Initial_Rod, char Destination_Rod, char Aux_Rod)
     {
-
-        if (n == 1)
-        {
-            System.out.println("Move disk 1 from rod " +  source + " to rod " + to_rod);
+        // Base Case for recursion function
+        if (Dish_Number == 1) {
+            System.out.println("Move disk 1 from rod " +  Initial_Rod + " to rod " + Destination_Rod);
             return;
         }
-        towerOfHanoi(n-1, source, aux_rod, to_rod);
-        System.out.println("Move disk " + n + " from rod " +  source + " to rod " + to_rod);
-        towerOfHanoi(n-1, aux_rod, to_rod, source);
+
+        //Recursive Functions
+
+        towerOfHanoi(Dish_Number-1, Initial_Rod, Aux_Rod, Destination_Rod);
+        System.out.println("Move disk " + Dish_Number + " from rod " +  Initial_Rod + " to rod " + Destination_Rod);
+        towerOfHanoi(Dish_Number-1, Aux_Rod, Destination_Rod, Initial_Rod);
     }
 
-    //  Driver method
+    //  Main Method
     public static void main(String args[])
     {
-        int n = 4; // Number of disks
-        towerOfHanoi(n, 'A', 'C', 'B');  // A, B and C are names of rods
+        int Dish_Number = 4; // Number of disks
+        towerOfHanoi(Dish_Number, 'A', 'C', 'B');  // A, B and C are names of rods
     }
 }
