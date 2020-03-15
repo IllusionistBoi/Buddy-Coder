07bac8bc7c86226af08c4833bf91a2b0e8794942
IllusionistBoi <ronit.dahiya@ucdconnect.ie>
2020-03-15 07:38:06 +0000

    Simple Equality Check Program

---
+++ SumCheck/src/com/rd/Main.java
@@ -0,0 +1,9 @@
+package com.rd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean output = SumCheck.hasEqualSum(1,-1,0);
+        System.out.println(output);
+    }
+}
---
+++ SumCheck/src/com/rd/SumCheck.java
@@ -0,0 +1,10 @@
+package com.rd;
+
+public class SumCheck {
+    public static boolean hasEqualSum(int a, int b, int sum) {
+        if( (a+ b) == sum )
+            return  true;
+        else
+            return false;
+    }
+}
